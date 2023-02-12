05da12d3ff73fe66a724784fedd22d8a96a2e3c5
bruno.duarte <bruno.duarte@somosdx.co>
2023-02-12 18:06:28 -0300

    Adicionando modulo de banco de dados

--- src/commons/config/config.service.ts
+++ src/commons/config/config.service.ts
@@ -1,7 +1,10 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10) || 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
   },
 });
--- src/commons/database/database.module.ts
+++ src/commons/database/database.module.ts
@@ -3,19 +3,19 @@ import { DataSource } from 'typeorm';
 import ConfigModule from '../config/config.module';
 import { ConfigService } from '@nestjs/config';
 
-TypeOrmModule.forRootAsync({
+export default TypeOrmModule.forRootAsync({
   imports: [ConfigModule],
   inject: [ConfigService],
   // Use useFactory, useClass, or useExisting
   // to configure the DataSourceOptions.
   useFactory: (configService: ConfigService) => ({
     type: 'mysql',
-    host: configService.get('HOST'),
-    port: +configService.get('PORT'),
-    username: configService.get('USERNAME'),
-    password: configService.get('PASSWORD'),
-    database: configService.get('DATABASE'),
-    entities: [],
+    host: configService.get('database.host'),
+    port: +configService.get('database.port'),
+    username: configService.get('database.username'),
+    password: configService.get('database.password'),
+    database: configService.get('database.name'),
+    autoLoadEntities: true,
     synchronize: true,
   }),
   // dataSource receives the configured DataSourceOptions
