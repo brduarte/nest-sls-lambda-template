15068209a0f09e5dc02401bcef348d786b1316f2
bruno.duarte <bruno.duarte@somosdx.co>
2023-04-02 22:49:01 -0300

    Salvando trabalho

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import DatabaseModule from './commons/database/database.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './user.entity';
-import { OpenaiModule } from './commons/providers/openai/openai.module';
-import { GoogleModule } from './commons/providers/google/google.module';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    TypeOrmModule.forFeature([UserEntity]),
-    OpenaiModule,
-    GoogleModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule],
 })
 export class AppModule {}
